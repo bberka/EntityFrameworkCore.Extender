47529efea65884d4ad2b4313dd22b35ddbfca394
bberka <berkaybyr@icloud.com>
2023-08-02 19:42:05 +0300

    single entity

---
+++ src/EntityFrameworkCore.Extender/Abstract/SingleEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityFrameworkCore.Extender.Abstract;
+
+public abstract class SingleEntity
+{
+  private bool _key = true;
+  /// <summary>
+  /// Key is always true and can't be changed. Single entity is forced in database
+  /// </summary>
+  [Key]
+  public bool Key {
+    get => _key;
+    set => _key = true;
+  }
+
+}
