97d327fabd505ce0249fb5a1e03af90fbf5b7acc
bberka <berkaybyr@icloud.com>
2023-08-06 19:54:51 +0300

    minor file removed

--- src/EntityFrameworkCore.Extender/Example.cs
+++ src/EntityFrameworkCore.Extender/Example.cs
@@ -1,11 +1,11 @@
-﻿using EntityFrameworkCore.Extender.Abstract;
-
-namespace EntityFrameworkCore.Extender;
-
-public abstract class MyBaseEntity : IEntity, IEntityBase<Guid>,IEntityConcurrencyProps,IEntityDefaultProps
-{
-  public abstract Guid Id { get; set; }
-  public abstract byte[] Version { get; set; }
-  public abstract DateTime CreatedAt { get; set; }
-  public abstract DateTime? UpdatedAt { get; set; }
-}
+﻿// using EntityFrameworkCore.Extender.Abstract;
+//
+// namespace EntityFrameworkCore.Extender;
+//
+// public abstract class MyBaseEntity : IEntity, IEntityBase<Guid>,IEntityConcurrencyProps,IEntityDefaultProps
+// {
+//   public abstract Guid Id { get; set; }
+//   public abstract byte[] Version { get; set; }
+//   public abstract DateTime CreatedAt { get; set; }
+//   public abstract DateTime? UpdatedAt { get; set; }
+// }
